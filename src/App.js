162ef83ddc849cb3ab8bba17a9b98e9e9dc12232
maks0bs <maksthebro@173gmail.com>
2020-03-23 18:46:55 +0100

    adding test data for deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        //temporary state
+        this.state = {
+            text: ''
+        }
+    }
+
+
+    componentDidMount(){
+        //temporary test data to check networking
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({
+                text: JSON.stringify(data);
+            })
+        })
+    }
+
     render() {
         return (
             <div>
-                Sample text wow
+                {this.state.text}
             </div>
         );
     }
