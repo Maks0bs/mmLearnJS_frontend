4843981227be11d91e0ff0ff91fc58b7efec79f3
maks0bs <maksthebro@173gmail.com>
2020-08-25 17:26:59 +0200

    Added rookout

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './store'
+import rookout from 'rookout';
+
+rookout.start({
+	token: 'a8651ca79fa23c430f06c51c9e837bda56d9841f1c94b9dcdbb71988599a4cfb',
+	labels: {
+		env: 'dev'
+	}
+})
 
 ReactDOM.render(
   	<React.StrictMode>
