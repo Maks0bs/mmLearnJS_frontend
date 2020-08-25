ef551cf73fc023107ceb3fc5850b3bc6f2b5e6b1
maks0bs <maksthebro@173gmail.com>
2020-08-25 17:49:19 +0200

    Rookout fixed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './store'
-import rookout from 'rookout';
 
-rookout.start({
-	token: 'a8651ca79fa23c430f06c51c9e837bda56d9841f1c94b9dcdbb71988599a4cfb',
-	labels: {
-		env: 'dev'
-	}
-})
+if (process.env.NODE_ENV === 'production'){
+	let rookout = require('rookout');
+
+	console.log('starting the app boi');
+
+	rookout.start({
+		token: 'a8651ca79fa23c430f06c51c9e837bda56d9841f1c94b9dcdbb71988599a4cfb',
+		labels: {
+			env: 'dev'
+		}
+	})
+}
+
 
 ReactDOM.render(
   	<React.StrictMode>
