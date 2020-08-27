d1bf65e972cfb185ae97ab851498cb34347b77df
maks0bs <maksthebro@173gmail.com>
2020-08-27 16:01:21 +0200

    Completed basic docs for common components and services

--- src/components/services/helpers.js
+++ src/components/services/helpers.js
@@ -3,7 +3,7 @@
  * @param {any[]}list array to reorder
  * @param {number} startIndex the index of the element to remove from the list and put to another position
  * @param {number} endIndex the position we want to insert the new element into
- * @returns {[any]} deep copy of the reordered given array
+ * @returns {any[]} deep copy of the reordered given array
  */
 export let reorderArray = (list, startIndex, endIndex) => {
     let result = Array.from(list);
--- src/services/reducer.js
+++ src/services/reducer.js
@@ -5,7 +5,13 @@ import { combineReducers } from "redux";
 import servicesReducer from './main/reducer'
 
 
-
+/**
+ *
+ * @description `views` - the reducer for actions inside all views inside the app
+ * @description `components` - the reducer to for components, that are commonly used across the app
+ * @description `services` - the basic information from the api: authenticated user...
+ * @description `routing` - the reducer used for navigation, primarily for the navigation bars
+ */
 export default combineReducers({
 	views: viewsReducer,
 	services: servicesReducer,
